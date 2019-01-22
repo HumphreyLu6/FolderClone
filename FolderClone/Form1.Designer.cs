7d7e3ca74c2843f945389544adeaba6546f6c5d2
Humphrey Lu <zlu@ualberta.ca>
2019-01-21 17:09:31 -0700

    Update intial project

--- FolderClone/Form1.Designer.cs
+++ FolderClone/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(84, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Clone to...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Location = new System.Drawing.Point(87, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 24);
+            this.label5.TabIndex = 6;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(60, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(165, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(379, 424);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- FolderClone/Form1.cs
+++ FolderClone/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,73 @@ namespace FolderClone
 {
     public partial class Form1 : Form
     {
+        private FolderBrowserDialog eFolderBrowserDialog;
+        private string eSelectedFolderPath;
+        private string eTargetFolderPath;
+        private List<string> folders;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.Width = 418;
+            this.Height = 440;
+            this.eFolderBrowserDialog = new FolderBrowserDialog();
+
+            button1.Width = button2.Width = 50;
+            button1.Height = button2.Height = 30;
+         
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            eFolderBrowserDialog.Description = "Select the directory that you want to clone";
+            eFolderBrowserDialog.ShowNewFolderButton = false;
+            eFolderBrowserDialog.SelectedPath = @"c:\";
+            
+            DialogResult result = eFolderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.eSelectedFolderPath = label1.Text = eFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = eFolderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.eTargetFolderPath = label5.Text = eFolderBrowserDialog.SelectedPath; 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CreateDirectories(this.eSelectedFolderPath, this.eTargetFolderPath);
+            MessageBox.Show("Folders have been created.");
+            this.Dispose();
+        }
+
+        private void CreateDirectories(string folderPath, string targetPath)
+        {
+            string folderName = Path.GetFileName(folderPath);
+            string newTargetFolder = Path.Combine(targetPath, folderName);
+            Directory.CreateDirectory(newTargetFolder);
+
+            string[] subDirectoryFolders = Directory.GetDirectories(folderPath);
+            if (subDirectoryFolders.Length > 0) { 
+                foreach (string folder in subDirectoryFolders)
+                {
+                    CreateDirectories(folder, newTargetFolder);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
